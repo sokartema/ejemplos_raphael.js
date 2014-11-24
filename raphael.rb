f92a8bf2d465278e0669d968e576175f89c6e4c6
Jazer Abreu <sokartema@gmail.com>
2014-11-24 20:19:45 +0000

    Cambiada interfaz y añadiendo ejemplos

--- raphael.rb
+++ raphael.rb
@@ -5,10 +5,15 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'haml'
 
+array = ["yellow","orange","red","purple","blue","turquoise","green"]
+
 set :environment , :development
 
 get '/' do
 
+  @title = "Index"
+
+  @color = array[rand(array.size)]
 
   haml :index
 
